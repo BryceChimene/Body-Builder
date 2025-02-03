76933017b08ff187101d7997d831528382682751
Bryce Chimene <brycechimene@yahoo.com>
2025-02-03 16:08:48 -0600

    Finish header with muscle group nav

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 // Script to pass the data-value of clicked muscle-group to the server
-
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('g').forEach((group) => {
       group.addEventListener('click', () => {
@@ -7,4 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
           window.location.href = `/?target=${target}`; // Redirect to the server with the target query
       });
   });
+
+  document.querySelectorAll('a').forEach((group) => {
+    group.addEventListener('click', () => {
+        const target = group.getAttribute('data-value'); // Retrieve the target value
+        window.location.href = `/?target=${target}`; // Redirect to the server with the target query
+    });
+});
 });
